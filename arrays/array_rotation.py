e54a8b6ef799410b27054e223d031337a5613942
Sachin-Naik-2000 <85633952+Sachin-Naik-2000@users.noreply.github.com>
2021-07-03 14:45:47 +0530

    Create array_rotation.py

---
+++ arrays/array_rotation.py
@@ -0,0 +1,7 @@
+n=int(input("enter the no fo rotation : "))
+
+list=[1,2,3,4,5,6,7]
+
+print("before rotation: {}".format(list))
+
+print("after rotation: {}".format(list[n:]+list[:n]))     #n for clock wise rotation and -n for anticlock wise rotation
